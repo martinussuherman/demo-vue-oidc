7465cfed780246cbdabed02c583b4bb7d7e101c1
Martinus Suherman <martinus.suherman@gmail.com>
2021-02-12 16:07:31 +0700

    Refactor
    - Fix getter and action binding not working
    - Add comment for non matched type definition in vuex-oidc
    - Update template

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,68 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <button
-        v-if="oidcIsAuthenticated"
-        @click="signOutOidc"
-      >
-        {{ oidcUser.profile.email }} - Logout
-      </button>
+      <a v-if="oidcIsAuthenticated" href @click.prevent="signOut">Sign out - {{ oidcUser.email }}</a>
     </div>
+
     <router-view/>
   </div>
 </template>
 
 <script lang="ts">
+import { mapActions, mapGetters } from 'vuex'
 import { Component, Vue } from 'vue-property-decorator'
-import { namespace } from 'vuex-class'
-import { User } from 'oidc-client'
+import { VuexOidcStoreActions, VuexOidcStoreGetters } from 'vuex-oidc'
+import { Profile } from 'oidc-client'
 
-const oidcStore = namespace('oidcStore')
+/** https://stackoverflow.com/questions/60099323/how-to-use-mapactions-with-vue-typescript-class-component */
+@Component({
+  computed: {
+    ...mapGetters(
+      'oidcStore',
+      {
+        oidcIsAuthenticated: 'oidcIsAuthenticated',
+        oidcUser: 'oidcUser'
+      }
+    )
+  },
+  methods: {
+    ...mapActions(
+      'oidcStore',
+      {
+        authenticateOidcPopup: 'authenticateOidcPopup',
+        removeOidcUser: 'removeOidcUser',
+        signOutOidc: 'signOutOidc'
+      }
+    )
+  }
+})
 
-@Component
 export default class App extends Vue {
-  @oidcStore.Getter
-  public oidcIsAuthenticated!: boolean
+  oidcIsAuthenticated!: VuexOidcStoreGetters['oidcIsAuthenticated'];
 
-  @oidcStore.Getter
-  public oidcUser!: User | null
+  /** oidcUser in vuex-oidc returns any  */
+  // oidcUser!: VuexOidcStoreGetters['oidcUser'];
+  oidcUser!: Profile;
 
-  @oidcStore.Action
-  public signOutOidc!: (payload?: object) => void
+  authenticateOidcPopup!: VuexOidcStoreActions['authenticateOidcPopup'];
+
+  /** removeOidcUser in vuex-oidc returns void  */
+  // signOutOidc!: VuexOidcStoreActions['signOutOidc'];
+  signOutOidc!: (payload?: object) => Promise<void>;
+
+  /** removeOidcUser in vuex-oidc returns void  */
+  //  removeOidcUser!: VuexOidcStoreActions['removeOidcUser'];
+  removeOidcUser!: () => Promise<void>;
+
+  hasAccess () {
+    return this.oidcIsAuthenticated || this.$route.meta.isPublic
+  }
+
+  signOut () {
+    this.signOutOidc().then(() => {
+      this.$router.push('/')
+    })
+  }
 }
 </script>
 
