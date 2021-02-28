f6fa94e5c44938515a8220cdd452aca1eaea7193
Martinus Suherman <martinus.suherman@gmail.com>
2021-02-28 12:59:32 +0700

    Add build rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/demo-vue-oidc/',
-  plugins: [createVuePlugin(/*options*/)]
+  plugins: [createVuePlugin(/*options*/)],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        notFound: resolve(__dirname, '404.html'),
+        oidcRenew: resolve(__dirname, 'silent-renew-oidc.html')
+      }
+    }
+  }
 })
