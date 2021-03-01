e543a9628a8ac0e2fe7b3c604824d34f83ef154a
Martinus Suherman <martinus.suherman@gmail.com>
2021-03-01 11:38:19 +0700

    Update vite config
    - Add resolve alias
    - Format with prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { createVuePlugin } from "vite-plugin-vue2";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/demo-vue-oidc/',
+  resolve: {
+    alias: {
+      "@": `${resolve(__dirname, "src")}`,
+    },
+  },
+  base: "/demo-vue-oidc/",
   plugins: [createVuePlugin(/*options*/)],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        notFound: resolve(__dirname, '404.html'),
-        oidcRenew: resolve(__dirname, 'silent-renew-oidc.html')
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "index.html"),
+        notFound: resolve(__dirname, "404.html"),
+        oidcRenew: resolve(__dirname, "silent-renew-oidc.html"),
+      },
+    },
+  },
+});
