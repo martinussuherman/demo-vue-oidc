98f8ba7aec6eefea0baf826a4aed61e8eca5fe38
martinussuherman <martinussuherman@users.noreply.github.com>
2021-11-13 01:55:59 +0000

    deploy: 78371aa92e7a74d68e6773692fc4ad3a1c430fd5

--- assets/main.47b0012a.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill.b7f2da20.js";import"./main.48d92119.js";import"./vendor.0f2fe268.js";
---
+++ assets/oidcRenew.25714e1f.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.b7f2da20.js";import{y as l}from"./vendor.49608252.js";l();
